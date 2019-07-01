53ec0be28ecc1684783bc48dc7b73624988e7953
Igor Lapin <igor.lapin73@gmail.com>
2019-07-01 16:20:11 +0300

    Fix switching to Immersive mode

--- app/src/main/java/ilapin/earth/ui/compass/CompassActivity.kt
+++ app/src/main/java/ilapin/earth/ui/compass/CompassActivity.kt
@@ -41,14 +41,6 @@ class CompassActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compass)
 
-        containerLayout.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-            View.SYSTEM_UI_FLAG_FULLSCREEN or
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-
         cameraPermissionResolver = CameraPermissionResolver(LocalCameraPermissionRepository(this))
 
         permanentSubscriptions.add(cameraPermissionResolver.permission.subscribe { permission ->
@@ -116,6 +108,14 @@ class CompassActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+
+        containerLayout.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
         pausableSubscriptions.add(magneticFieldRepository.magneticField().subscribe { magneticField ->
             magneticFieldView.text = getString(
